d7a966ff53087fad7a531c3de710e216e71b533b
Martin Waby Smoger <mvsmoger@gmail.com>
2016-02-20 17:53:29 -0500

    CSV

--- App1/SlidingTabsFragment.cs
+++ App1/SlidingTabsFragment.cs
@@ -131,10 +131,191 @@ namespace Scouter
                 return files;
             }
 
-            private void WriteFile(string contents)
+            private void WriteFile(string contents, string fileName)
             {
                 var path = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDocuments);
+                var fullPath = Path.Combine(path.ToString() + fileName + ".csv");
 
+                using (var streamWriter = new StreamWriter(fullPath, false))
+                {
+                    streamWriter.WriteLine(contents);
+                }
+            }
+            
+            private string ReadFile(string fileName)
+            {
+                var path = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDocuments);
+                var filePath = Path.Combine(path.ToString(), fileName);
+                string text = File.ReadAllText(path.ToString());
+                return text;
+            }
+
+            private string defenseFromCode(char code)
+            {
+                switch (code.ToString())
+                {
+                    case "0":
+                        return "Portcullis";
+                    case "1":
+                        return "Cheval de Frise";
+                    case "2":
+                        return "Ramparts";
+                    case "3":
+                        return "Moat";
+                    case "4":
+                        return "Drawbridge";
+                    case "5":
+                        return "Sally Port";
+                    case "6":
+                        return "Rock Wall";
+                    case "7":
+                        return "Rough Terrain";
+                    case "8":
+                        return "Low Bar";
+                }
+                return null;
+            }
+            private string defense(char code)
+            {
+                switch (code.ToString())
+                {
+                    case "0":
+                        return "Portcullis";
+                    case "1":
+                        return "Cheval de Frise";
+                    case "2":
+                        return "Ramparts";
+                    case "3":
+                        return "Moat";
+                    case "4":
+                        return "Drawbridge";
+                    case "5":
+                        return "Sally Port";
+                    case "6":
+                        return "Rock Wall";
+                    case "7":
+                        return "Rough Terrain";
+                    case "8":
+                        return "Low Bar";
+                }
+                return null;
+            }
+
+            private string FileToCSVFormat(string fileName, int mode)
+            {
+                string text = ReadFile(fileName);
+                char[] content = text.ToCharArray();
+                char[] file = fileName.ToCharArray();
+                char[] teamNumber = new char[4];
+                char[] matchNumber = new char[2];
+                
+                for(int i = 0; i < file.Length; i++)//Gets team number and match number from file name
+                {
+                    if(i < 4)
+                    {
+                        teamNumber[i] = file[i];
+                    }
+                    else
+                    {
+                        matchNumber[i - 4] = file[i];
+                    }
+                }
+                string scoutName = "unknown";
+                switch(content[24].ToString())
+                {
+                    case "a":
+                        scoutName = null;
+                        break;
+                    case "b":
+                        scoutName = null;
+                        break;
+                    case "c":
+                        scoutName = null;
+                        break;
+                    case "d":
+                        scoutName = null;
+                        break;
+                    case "e":
+                        scoutName = null;
+                        break;
+                    case "f":
+                        scoutName = null;
+                        break;
+                    case "g":
+                        scoutName = null;
+                        break;
+                    case "h":
+                        scoutName = null;
+                        break;
+                    case "i":
+                        scoutName = null;
+                        break;
+                    case "j":
+                        scoutName = null;
+                        break;
+                    case "k":
+                        scoutName = null;
+                        break;
+                    case "l":
+                        scoutName = null;
+                        break;
+                    case "m":
+                        scoutName = null;
+                        break;
+                    case "n":
+                        scoutName = null;
+                        break;
+                    case "o":
+                        scoutName = null;
+                        break;
+                    case "p":
+                        scoutName = null;
+                        break;
+                    case "q":
+                        scoutName = null;
+                        break;
+                    case "r":
+                        scoutName = null;
+                        break;
+                    case "s":
+                        scoutName = null;
+                        break;
+                    case "t":
+                        scoutName = null;
+                        break;
+                    case "u":
+                        scoutName = null;
+                        break;
+                    case "v":
+                        scoutName = null;
+                        break;
+                    case "w":
+                        scoutName = null;
+                        break;
+                    case "x":
+                        scoutName = null;
+                        break;
+                    case "y":
+                        scoutName = null;
+                        break;
+                    case "z":
+                        scoutName = null;
+                        break;
+                }
+                string txtMatch = new string(matchNumber);
+                string txtTeam = new string(teamNumber);
+                string newCSVText = content[29].ToString() + "," + scoutName + "," + txtMatch +
+                    "," + txtTeam + "," + content[27].ToString() + "," + content[18].ToString() + ","
+                    + content[16].ToString() + "," + content[19].ToString() + "," + content[17].ToString()
+                    + content[22].ToString() + "," + content[20].ToString() + "," + content[23].ToString() + ","
+                    + content[21].ToString() + "," + content[24].ToString() + "," + content[25].ToString() + ","
+                    + content[26].ToString() + "," + defense(content[0]) + "," + content[1].ToString() + "," +
+                    content[2].ToString() + "," + content[3].ToString() + "," + defenseFromCode(content[4]) + ","
+                    + content[5].ToString() + "," + content[6].ToString() + "," + content[7].ToString() + ","
+                    + defenseFromCode(content[8]) + "," + content[9].ToString() + "," + content[10].ToString()
+                    + content[11].ToString() + "," + defenseFromCode(content[12]) + "," + content[13].ToString() +
+                    content[14].ToString() + content[15].ToString();
+                return newCSVText;
             }
         }
     }
